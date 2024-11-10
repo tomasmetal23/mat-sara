da2ae60a1ca041d094374d65ad90705d7636c04d
unknown <servicracm@gmail.com>
2024-11-10 19:02:59 -0400

    actualizacion de tabla

---
+++ js/menu.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+		$("#accion").click(function(){
+		location.href="juego.html";
+				
+		})		
+	
+		$("#contenido").click(function(){
+		location.href="contenido.html";
+		})
+	
+		$("#tabla").click(function(){
+		location.href="tabla.html";
+		})
+		
+		$("#tabla2").click(function(){
+		location.href="tabla.html";
+		})
+		
+		$("#aempresa").click(function(){
+
+			
+		window.open('https://www.geogebra.org/graphing?lang=es', '_blank', 'location=no');
+
+		})
+	
+		
+	})
